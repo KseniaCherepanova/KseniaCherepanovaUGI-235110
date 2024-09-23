28b2590e8922ef1a8272bde882f0645391d0ecb0
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2024-09-23 17:00:50 +0500

    Задание 1 было выполнено, написано стихотворение, заглавие зеленого цвета, а основной текст желтого цвета.

---
+++ taskzerooneproject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskzerooneproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Малинка");
+
+            Console.WriteLine();
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Что очень любит детвора");
+            Console.WriteLine("Срывать с колючего куста");
+            Console.WriteLine("И собирать в корзинку?");
+            Console.WriteLine("Конечно же — малинку!");
+
+            Console.ResetColor();
+
+
+            Console.ReadKey();
+          
+        }
+    }
+}
