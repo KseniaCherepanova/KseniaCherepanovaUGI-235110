4c7a9119a580c5fbba0631aa4c34863625432709
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2024-10-07 16:54:23 +0500

    Задание 4 вариант 18 сделано Черепанова Ксения УГИ-235110

---
+++ domzadanietri3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domzadanietri3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числитель: ");
+            var m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите знамениатель: ");
+            var n = double.Parse(Console.ReadLine());
+
+            double result = m / n;
+
+
+            Console.ReadLine(result); 
+            
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ zad071024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad071024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число x");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyMeaning(x);   
+            Console.WriteLine($"F(x) = {y}");
+            
+            Console.ReadKey();  
+        }
+        static double MyMeaning(double x)
+        {
+            //throw new NotImplementedException();
+ 
+        return ((((1/(x*x + 1))+2)/((x * x + 2) + 3))/(x * x + 3));
+        }
+    }
+}
