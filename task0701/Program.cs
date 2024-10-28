ab0ed09e6f762dc3f77019e250213fc5db235119
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2024-10-28 15:14:02 +0500

    сделаны два задания 7.1 и 7.2

---
+++ task0701/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task0701
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+
+            if (IsReshenie(m, n))
+                Console.WriteLine("Удтверждение истинно");
+            else
+                Console.WriteLine("Удтверждение ложно");
+
+            Console.ReadKey();  
+        }
+
+        static bool IsReshenie(int m, int n)
+        {
+            return m % 2 == 0 && n % 2 == 0 && (m * n) % 2 != 0;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ task0703/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task0703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Введите позицию белой фигуры");
+
+            var whitePawnPosition = Console.ReadLine();
+            int whitePawnRow, whitePawnColumn;
+
+            DecodePosition(whitePawnPosition, out whitePawnRow, out whitePawnColumn);
+
+            Console.WriteLine($"({whitePawnRow}; {whitePawnColumn})");
+
+            Console.ReadLine(); 
+        }
+
+        static void DecodePosition(string position, out int x , out int y)
+        {
+            x = int.Parse(position[1].ToString());
+            y = (int)position[0] - 0x60;
+        }
+    }
+}
