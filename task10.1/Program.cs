aaf5c98a4442a833a83140fe656cf9dbf170b856
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2024-12-08 18:47:37 +0500

    task 10.1 completed

---
+++ task10.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+
+           
+            while (true)
+            {
+                Console.Write("Введите число k (от 2 до 9): ");
+                string input = Console.ReadLine();
+
+                if (TryCheckOutRange(input, out k))
+                {
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Введите целое число в диапазоне от 2 до 9.");
+                }
+            }
+
+           
+            Console.WriteLine($"Таблица умножения на {k}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{k} x {i} = {k * i}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool TryCheckOutRange(string input, out int number)
+        {
+            return int.TryParse(input, out number) && number >= 2 && number <= 9;
+        }
+    }
+}
