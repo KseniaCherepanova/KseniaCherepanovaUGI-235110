1ff95c2f4175c8e3a629a19b51763d6bc23f0a81
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2024-12-08 20:15:09 +0500

    10.5 is completed

---
+++ task10.5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x (процент увеличения площади): ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y (процент средней урожайности): ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введите n (требуемый общий урожай в тоннах): ");
+            double n = double.Parse(Console.ReadLine());
+
+            double area = 10; // начальная площадь
+            double yieldPerHectare = 20; // начальная урожайность
+            double totalYield = 0;
+            int year = 0;
+
+            while (totalYield < n * 10) // Переводим тонны в центнеры
+            {
+                year++;
+                totalYield += area * yieldPerHectare;
+                area += area * (x / 100);
+                yieldPerHectare += yieldPerHectare * (y / 100);
+            }
+
+            Console.WriteLine($"Общий урожай превысит {n} тонн в {year}-й год.");
+            Console.ReadKey();
+        }
+    }
+}
